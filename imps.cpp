127e55a2ca08cb7858b9f3c35448fb85c86cd307
Henrique Bissoli Silva <emp.shad@gmail.com>
2016-07-01 00:14:45 -0300

    Update imps.cpp

--- imps.cpp
+++ imps.cpp
@@ -1,5 +1,10 @@
 #include "crow.h"
 
+/*
+    Crow: https://github.com/ipkn/crow
+    compiler: g++ -O3 -std=c++14 imps.cpp -lboost_thread -lboost_system -pthread -o imps
+*/
+
 int main(){
     crow::SimpleApp app;
     CROW_ROUTE(app, "/")([](){
