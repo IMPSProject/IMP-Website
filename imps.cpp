bf15a98ddf77616d2ddbeb84d19ebb763b48f8aa
Henrique Bissoli Silva <emp.shad@gmail.com>
2016-07-01 11:41:55 -0300

    Update imps.cpp

--- imps.cpp
+++ imps.cpp
@@ -1,4 +1,5 @@
-#include "crow.h"
+//#define CROW_ENABLE_SSL
+#include "crow_all.h"
 
 /*
     Crow: https://github.com/ipkn/crow
@@ -6,9 +7,47 @@
 */
 
 int main(){
-    crow::SimpleApp app;
-    CROW_ROUTE(app, "/")([](){
-        return "Hail IMPS!";
-    });
-    app.port(80).multithreaded().run();
+	crow::SimpleApp app;
+	crow::mustache::set_base(".");
+
+	// Home
+	CROW_ROUTE(app, "/")([](){
+		crow::mustache::context ctx;
+        return crow::mustache::load("home.html").render();
+	});
+
+	// Lagertha
+	CROW_ROUTE(app, "/admin/")([](){
+		return "Lagertha.";
+	});
+
+	// Close session
+	CROW_ROUTE(app, "/admin/sair")([](){
+		return "Sair do Lagertha.";
+	});
+
+	// Client Admin
+	CROW_ROUTE(app, "/publicidade/")([](){
+		return "Publicidade.";
+	});
+
+	// Get IP
+	CROW_ROUTE(app, "/ip")([](){
+		return "IP";
+	});
+	
+	// POST
+	CROW_ROUTE(app, "/post/").methods(crow::HTTPMethod::POST)([](const crow::request& req){
+		auto x = crow::json::load(req.body);
+		if(!x) return crow::response(400);	
+		return "Post";
+	});
+	
+	// Log
+	crow::logger::setLogLevel(crow::LogLevel::DEBUG);
+	
+	// SSL/HTTPS .ssl_file("test.crt", "test.key")
+	
+	// App
+	app.port(80).multithreaded().run();
 }
