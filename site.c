894371f3a46f07d546da2016c510fb34293fa6ac
Henrique Bissoli Silva <emp.shad@gmail.com>
2016-06-30 21:53:11 -0300

    Update site.c

--- site.c
+++ site.c
@@ -4,14 +4,14 @@
 int	page(struct http_request *);
 int page(struct http_request *req){
 	char hello[] = "<!DOCTYPE html>"
-						"<html>"
-							"<head>"
-								
-							"</head>"
-							"<body>"
-								"IMPS"
-							"</body>"
-						"</html>";
+			"<html>"
+				"<head>"
+						
+				"</head>"
+				"<body>"
+					"IMPS"
+				"</body>"
+			"</html>";
 	http_response(req, 200, hello, sizeof(hello)-1);
 	return (KORE_RESULT_OK);
 }
